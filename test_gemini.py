a50f23bfc12eae44cc9ebd7f4aa17d25e15aed61
shivannad <shpa22csaiml@cmrit.ac.in>
2026-02-02 23:59:53 +0530

    Update API key for genai.Client initialization

--- test_gemini.py
+++ test_gemini.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyAtpEj0mn0UhgG7Jh_TEVnINjnz2EpJzAk")
+client = genai.Client(api_key="GEMINI_API_KEY")
 
 res = client.models.generate_content(
     model="gemini-2.5-pro",
